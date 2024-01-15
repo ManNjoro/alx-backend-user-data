ad1b5ca12c919319cc34828b7b48255e1c9da074
ManNjoro <elijohnmwoho@gmail.com>
2024-01-15 14:47:41 +0300

    Extract base64 encoded user and password from Authorization

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,3 +10,20 @@ class BasicAuth(Auth):
     """
     Implementation of the basic authentication method
     """
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """
+        Extracts base64 encoded user and password
+        from an Authorization header value
+        Args:
+        authorization_header (str): The HTTP Authorization header value
+        Returns:
+        Base64 part of the Authorization header for a Basic Authentication
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.split(" ")[0] == 'Basic':
+            return None
+        return authorization_header.split(" ")[1]
