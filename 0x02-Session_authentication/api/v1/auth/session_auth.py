9af6ba2bc59397ecedbd18e764551fe0f1268e65
ManNjoro <elijohnmwoho@gmail.com>
2024-01-17 20:10:12 +0300

    Add method to create a new API session for a given user ID and return its unique identifier.

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,8 +3,27 @@
 Implements session authentication
 """
 
+import uuid
 from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
     """Session Authentication Class."""
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Create a new API session for the given user ID and return its
+        unique identifier (UUID string)
+        Args:
+        user_id: The user's unique identifier. Defaults to `None`.
+        Returns:
+        A UUID string representing the newly-created session's
+        unique identifier.
+        """
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+        unique_id = str(uuid.uuid4())
+        self.user_id_by_session_id[unique_id] = user_id
+        return unique_id
