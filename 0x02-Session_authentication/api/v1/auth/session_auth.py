1abc0e7ad96e6ef831da5b652727a4425ce34c02
ManNjoro <elijohnmwoho@gmail.com>
2024-01-17 20:57:11 +0300

    Add `current_user` method to `SessionAuth` class.

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,7 +4,10 @@ Implements session authentication
 """
 
 import uuid
+
+from flask import abort, jsonify
 from api.v1.auth.auth import Auth
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -43,3 +46,12 @@ class SessionAuth(Auth):
         if not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """
+        Get the currently logged in user from the request object.
+        """
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        user = User.get(user_id)
+        return user
