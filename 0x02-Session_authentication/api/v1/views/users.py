74adc24c4a5d74f9ca3a1ca8f887aff7e0f30818
ManNjoro <elijohnmwoho@gmail.com>
2024-01-17 19:49:13 +0300

    Update user views to use `request.current_user`

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -30,6 +30,10 @@ def view_one_user(user_id: str = None) -> str:
     user = User.get(user_id)
     if user is None:
         abort(404)
+    if user_id == 'me' and request.current_user is None:
+        abort(404)
+    if user_id == 'me' and request.current_user is not None:
+        return jsonify(request.current_user.to_json())
     return jsonify(user.to_json())
 
 
@@ -120,3 +124,13 @@ def update_user(user_id: str = None) -> str:
         user.last_name = rj.get('last_name')
     user.save()
     return jsonify(user.to_json()), 200
+
+
+@app_views.route('/users/me', strict_slashes=False)
+def get_authenticated_user():
+    """ GET /api/v1/users/me
+    Return:
+    - Authenticated user object JSON represented
+    - 403 if no authenticated user
+    """
+    return jsonify(request.current_user.to_json())
