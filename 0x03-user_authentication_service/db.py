2ab20850d15d739b3510b40fea41767e7022fea8
ManNjoro <elijohnmwoho@gmail.com>
2024-01-22 17:40:05 +0300

    Update DB class to use `echo=False` and add `update_user` method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -18,7 +18,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -45,7 +45,7 @@ class DB:
         self._session.commit()
         return user
 
-    def find_user_by(self, **kwargs) -> User:
+    def find_user_by(self, **kwargs: dict) -> User:
         """Finds a single user by keyword arguments
         Raises:
         NoResultFound: If no user matches the criteria
@@ -59,3 +59,17 @@ class DB:
             raise NoResultFound()
         except InvalidRequestError:
             raise InvalidRequestError()
+
+    def update_user(self, user_id: int, **kwargs: dict) -> None:
+        """Updates a user in the database
+        Args:
+        user_id (int): The id of the user to be updated
+        kwargs: Keyword arguments for updating the user
+        """
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if hasattr(user, key):
+                setattr(user, key, value)
+                self._session.commit()
+            else:
+                raise ValueError()
