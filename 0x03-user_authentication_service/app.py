109ed519bc63e412dae66ad1bbd88c0ce400d150
ManNjoro <elijohnmwoho@gmail.com>
2024-01-22 19:05:10 +0300

    Add user registration endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,10 +3,14 @@
 Flask app
 """
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
+
+AUTH = Auth()
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.route('/')
@@ -14,6 +18,23 @@ def welcome():
     """Welcome page
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def users():
+    """Create a new user
+    """
+    if request.method == 'POST':
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        try:
+            user = AUTH.register_user(email, password)
+            if user:
+                response = {"email": f"{email}", "message": "user created"}
+                return jsonify(response)
+        except ValueError:
+            return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == '__main__':
