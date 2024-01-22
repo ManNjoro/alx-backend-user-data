974166d0a73feaa25552c72342d8a8f0e9c76c9d
ManNjoro <elijohnmwoho@gmail.com>
2024-01-22 19:26:48 +0300

    Add bcrypt dependency and implement password hashing and validation.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,6 +5,7 @@ auth module
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+import bcrypt
 
 
 def _hash_password(password: str) -> bytes:
@@ -13,7 +14,6 @@ def _hash_password(password: str) -> bytes:
     :param password: The plain text password to hash.
     :return: A byte string of the hashed password.
     """
-    import bcrypt
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password.encode('utf-8'), salt)
 
@@ -40,3 +40,19 @@ class Auth:
             user = self._db.add_user(
                 email=email, hashed_password=hashed_password)
             return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Checks whether login credentials are correct or not.
+        Args:
+        email (str): The email address of the user attempting to log in.
+        password (str): The password provided by the user.
+        Returns:
+        True if the login is successful, False otherwise.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                return bcrypt.checkpw(
+                    password.encode('utf-8'), user.hashed_password)
+        except Exception:
+            return False
