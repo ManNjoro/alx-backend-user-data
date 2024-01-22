f2e41348fac15ddbc5f06d2a79365344a46bd5c3
ManNjoro <elijohnmwoho@gmail.com>
2024-01-22 17:25:34 +0300

    Add `find_user_by` method to `DB` class to find a single user by keyword arguments. Handle `NoResultFound` and `InvalidRequestError` exceptions.

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
@@ -42,3 +44,18 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_user_by(self, **kwargs):
+        """Finds a single user by keyword arguments
+        Raises:
+        NoResultFound: If no user matches the criteria
+        InvalidRequestError: If wrong query arguments are passed
+        """
+        query = self._session.query(User).filter_by(**kwargs)
+        result = query.one()
+        try:
+            return result
+        except NoResultFound:
+            raise NoResultFound()
+        except InvalidRequestError:
+            raise InvalidRequestError()
