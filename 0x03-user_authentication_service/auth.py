1eb3443d9b304571ec9856e8795ab6b1f828fcfd
ManNjoro <elijohnmwoho@gmail.com>
2024-01-24 20:38:31 +0300

    Update password method added to Auth class.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -117,3 +117,22 @@ class Auth:
             return token
         except Exception:
             raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """
+        Update the user's password using a previously generated reset token.
+        If an invalid token is provided, this method raises a ValueError.
+        Args:
+        reset_token: A string that represents the password reset token.
+        password:   A string containing the new password.
+        Raises:
+        ValueError: If the provided token does not match any known tokens
+        in the system.
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            hashed_password = _hash_password(password)
+            self._db.update_user(
+                user.id, password=hashed_password, reset_token=None)
+        except Exception:
+            raise ValueError
